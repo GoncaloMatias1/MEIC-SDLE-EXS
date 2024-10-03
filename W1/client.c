c1ca7dd8677b915c18a58d0a606f11e55943e19f
GoncaloMatias1 <up202108703@g.uporto.pt>
2024-10-03 17:29:11 +0100

    added another server
    - Try adding another server and see the distribution of requests.
    - Try killing and re-starting the server

--- W1/client.c
+++ W1/client.c
@@ -5,20 +5,36 @@
 
 int main (void)
 {
-    printf ("Connecting to hello world server...\n");
+    printf ("Connecting to hello world servers...\n");
     void *context = zmq_ctx_new ();
-    void *requester = zmq_socket (context, ZMQ_REQ);
-    zmq_connect (requester, "tcp://localhost:5555");
-
+    
+    // Create two requesters, one for each server
+    void *requester1 = zmq_socket (context, ZMQ_REQ);
+    void *requester2 = zmq_socket (context, ZMQ_REQ);
+    
+    // Connect to both servers
+    zmq_connect (requester1, "tcp://localhost:5555");
+    zmq_connect (requester2, "tcp://localhost:5556");
+    
     int request_nbr;
     for (request_nbr = 0; request_nbr != 10; request_nbr++) {
         char buffer [10];
-        printf ("Sending Hello %d...\n", request_nbr);
-        zmq_send (requester, "Hello", 5, 0);
-        zmq_recv (requester, buffer, 10, 0);
-        printf ("Received World %d\n", request_nbr);
+        
+        // Send to first server
+        printf ("Sending Hello to server 1 (5555) - request %d...\n", request_nbr);
+        zmq_send (requester1, "Hello", 5, 0);
+        zmq_recv (requester1, buffer, 10, 0);
+        printf ("Received from server 1: World %d\n", request_nbr);
+        
+        // Send to second server
+        printf ("Sending Hello to server 2 (5556) - request %d...\n", request_nbr);
+        zmq_send (requester2, "Hello", 5, 0);
+        zmq_recv (requester2, buffer, 10, 0);
+        printf ("Received from server 2: World %d\n", request_nbr);
     }
-    zmq_close (requester);
+    
+    zmq_close (requester1);
+    zmq_close (requester2);
     zmq_ctx_destroy (context);
     return 0;
 }
--- W1/server.c
+++ W1/server.c
@@ -3,18 +3,32 @@
 #include <unistd.h>
 #include <string.h>
 #include <assert.h>
+#include <stdlib.h>
 
-int main (void)
+int main (int argc, char *argv[])
 {
+    // Check if port number is provided
+    if (argc != 2) {
+        printf("Usage: ./server <port>\n");
+        return 1;
+    }
+
     void *context = zmq_ctx_new ();
     void *responder = zmq_socket (context, ZMQ_REP);
-    int rc = zmq_bind (responder, "tcp://*:5555");
+    
+    // Create the connection string with the provided port
+    char connection[30];
+    sprintf(connection, "tcp://*:%s", argv[1]);
+    
+    int rc = zmq_bind (responder, connection);
     assert (rc == 0);
-
+    
+    printf("Server starting on port %s...\n", argv[1]);
+    
     while (1) {
         char buffer [10];
         zmq_recv (responder, buffer, 10, 0);
-        printf ("Received Hello\n");
+        printf ("Received Hello on port %s\n", argv[1]);
         sleep (1);
         zmq_send (responder, "World", 5, 0);
     }
